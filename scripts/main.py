af154369ef2b7ba79f0eafb9cf26515fc8d7cb92
Stewart Morris <stewshadow@gmail.com>
2023-05-03 13:39:11 -0400

    FEAT: actually makes video

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 import argparse
 import cv2
+import ffmpeg
 
 from style_transferrer import *
 from optical_flow import *
@@ -68,6 +69,9 @@ def main():
     cur_frame_grey = next_frame_grey
     cur_frame = next_frame
     count += 1
+
+    if (count == 3):
+      break
     
     # Again for drawing!
     # if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -75,6 +79,9 @@ def main():
   
   # cv2.destroyAllWindows()
   vidcap.release()
+
+  # ffmpeg -framerate 30 -pattern_type glob -i "frames/*.png" -c:v libx264 -crf 10 -pix_fmt yuv420p output/video.mp4 -y
+  ffmpeg.input('frames/*.png', pattern_type='glob', framerate=30).filter('deflicker', mode='pm', size=10).filter('scale', size='hd1080', force_original_aspect_ratio='increase').output('output/movie.mp4', crf=20, preset='slower', movflags='faststart', pix_fmt='yuv420p').run()
 
 if __name__ == "__main__":
   ARGS = parse_args()
